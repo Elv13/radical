9b9c96be1b9c4d5f6de0c6442699eb5219f9412f
Emmanuel Lepage Vallee <elv1313@gmail.com>
2014-03-22 17:03:04 -0400

    Fix a bug when table (widgets) are resized

--- widgets/table.lua
+++ widgets/table.lua
@@ -15,11 +15,12 @@ local function textbox_draw(self, w, cr, width, height)
   wibox.widget.textbox.draw(self, w, cr, width, height)
 end
 
-local function create_textbox(w,col_c,has_v_header,row_height)
+local function create_textbox(w,col_c,col,has_v_header,row_height)
   local t = wibox.widget.textbox()
-  t.fit = function(...)
-    local fw,fh = wibox.widget.textbox.fit(...)
-    return w/(col_c + (has_v_header and 1 or 0)),row_height or fh
+
+  t.fit = function(s,w2,h)
+    local fw,fh = wibox.widget.textbox.fit(s,w2,h)
+    return (w2/(col_c+2 - col)),row_height or fh
   end
   t.draw = textbox_draw
   t:set_align("center")
@@ -33,12 +34,12 @@ local function create_h_header(main_l,cols,w,args)
     bg:set_bg(beautiful.menu_bg_header or beautiful.fg_normal)
     bg:set_widget(row_l)
     if args.v_header then
-      local t = create_textbox(w,cols,args.v_header ~= nil,args.row_height)
+      local t = create_textbox(w,cols,1,args.v_header ~= nil,args.row_height)
       t:set_markup("<span color='".. beautiful.bg_normal .."'>--</span>")
       row_l:add(t)
     end
     for i=1,cols do
-      local t = create_textbox(w,cols,args.v_header ~= nil,args.row_height)
+      local t = create_textbox(w,cols,i+1,args.v_header ~= nil,args.row_height)
       t:set_markup("<span color='".. beautiful.bg_normal .."'>".. (args.h_header[i] or "-") .."</span>")
       row_l:add(t)
     end
@@ -62,12 +63,12 @@ local function new(content,args)
     return wibox.layout.fixed.fit(self,width,height)
   end
   create_h_header(main_l,cols,w,args)
-  
+
   local j,widgets =1,{}
   for k,v in  ipairs(content) do
     local row_l,row_w = wibox.layout.fixed.horizontal(),{}
     if args.v_header then
-      local t = create_textbox(w,cols,args.v_header ~= nil,args.row_height)
+      local t = create_textbox(w,cols,1,args.v_header ~= nil,args.row_height)
       t:set_markup("<span color='".. beautiful.bg_normal .."'>".. (args.v_header[j] or "-") .."</span>")
       local bg = wibox.widget.background()
       bg:set_bg(beautiful.menu_bg_header or beautiful.fg_normal)
@@ -75,7 +76,7 @@ local function new(content,args)
       row_l:add(bg)
     end
     for i=1,cols do
-      local t = create_textbox(w,cols,args.v_header ~= nil,args.row_height)
+      local t = create_textbox(w,cols,i+1,args.v_header ~= nil,args.row_height)
       t:set_text(v[i])
       row_l:add(t)
       row_w[#row_w+1] =t
