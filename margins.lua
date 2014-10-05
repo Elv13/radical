9bcfe953aef333c813d4deeb8e5503d2e2a80e46
Emmanuel Lepage Vallee <elv1313@gmail.com>
2014-10-05 17:53:26 -0400

    Add a dynamic margin tracker module

--- margins.lua
+++ margins.lua
@@ -2,10 +2,24 @@
 -- between the real wibox.widget.margins and the Radical menu.
 
 local setmetatable = setmetatable
+local awful = require("awful")
 local module = {}
 
-function module.create(widget,default)
-  local mt = setmetatable({merge = function(values)
+local function reset_margins(margins)
+  local widget = margins.widget
+  if widget and margins.defaults then
+    for k,v in pairs(margins.defaults) do
+      local f = widget["set_"..k:lower()]
+      if f then
+        f(widget,v)
+      end
+    end
+  end
+end
+
+local function new(widget,defaults)
+  local mt = setmetatable({defaults=awful.util.table.join(defaults,{}),widget=widget,reset=reset_margins,
+    merge = function(values)
     if values.left then
       widget:set_left(values.left)
     end
@@ -20,13 +34,24 @@ function module.create(widget,default)
     end
   end
   },{__newindex = function(tab, key,value)
-    widget["set_"..key](widget,value)
+    if key == "widget" then
+      rawset(tab,"widget",value)
+      reset_margins(tab)
+    elseif widget then
+      widget["set_"..key](widget,value)
+    else --TODO can't do this
+      defaults[key] = value
+    end
   end
   ,__index=function(table,key)
-    return widget[key]
-  end}}
+    local w = rawget(table,"widget")
+    return w and w[key] or 0 --widget["get_"..key](widget)
+  end})
+
+  reset_margins(mt)
+
   return mt
 end
 
-return module
+return setmetatable(module, { __call = function(_, ...) return new(...) end })
 -- kate: space-indent on; indent-width 2; replace-tabs on;
